ea6e9464d78bdef869fdbbb2b506dd096b58fcda
Alexey Luzin <zdytch@gmail.com>
2023-01-25 18:27:41 +0200

    Add db sample for conftest

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import asyncio
+from . import db as db_config
+
+
+@pytest.fixture()
+async def db():
+    async with db_config.Session() as session:
+        yield session
+
+
+@pytest.fixture(scope='session')
+def event_loop():
+    loop = asyncio.new_event_loop()
+
+    yield loop
+
+    loop.close()
+
+
+@pytest.fixture(autouse=True)
+async def setup_tables():
+    await db_config.drop_all()
+    await db_config.create_all()
+
+    yield
+
+    await db_config.drop_all()
